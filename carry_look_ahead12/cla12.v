5ebbbdd3d7ae7688bce3009816ba0e322c4f2a73
Mahdi <parchamimahdimohammad@gmail.com>
2023-03-06 11:03:03 +0330

    pictures and upgrade test-bench

--- carry_look_ahead12/cla12.v
+++ carry_look_ahead12/cla12.v
@@ -1,21 +1,79 @@
-module cla12 (a, b, cin, sum, cout);
-    input [11:0] a, b;
-    input cin;
-    output reg [11:0] sum;
-    output reg cout;
-    integer i, j, k;
-    reg [11:0] g,p;
-    reg [12:0] c;
-    reg temp;
-    always @(*) begin
-        c[0] = cin;
-        temp = 1;
-        for(i = 0; i < 12; i = i + 1) begin
-            g[i] = a[i] & b[i];
-            p[i] = a[i] ^ b[i];
-            c[i + 1] = g[i] | (p[i] & c[i]);
-            sum[i] = p[i] ^ c[i];
-        end
-        cout = c[12];
-    end
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   10:35:48 03/06/2023
+// Design Name:   cla12
+// Module Name:   C:/Users/mahdi/Desktop/cla12/cla12/cla12_tb.v
+// Project Name:  cla12
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: cla12
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module cla12_tb;
+
+	// Inputs
+	reg [11:0] a;
+	reg [11:0] b;
+	reg cin;
+
+	// Outputs
+	wire [11:0] sum;
+	wire cout;
+
+	// Instantiate the Unit Under Test (UUT)
+	cla12 uut (
+		.a(a), 
+		.b(b), 
+		.cin(cin), 
+		.sum(sum), 
+		.cout(cout)
+	);
+	initial begin 
+        cin = 0;
+        a = 12'd0;
+        b = 12'd0;
+        #100;
+        a = 12'd15;
+        b = 12'd1795;
+        #100;
+        a = 12'd12;
+        b = 12'd14;
+        #100;
+        a = 12'd7;
+        b = 12'd353;
+        #100;
+        a = 12'd11;
+        b = 12'd3311;
+        #100;
+		  cin = 1;
+		  a = 12'd0;
+        b = 12'd0;
+        #100;
+        a = 12'd15;
+        b = 12'd1795;
+        #100;
+        a = 12'd12;
+        b = 12'd14;
+        #100;
+        a = 12'd7;
+        b = 12'd353;
+        #100;
+        a = 12'd11;
+        b = 12'd3311;
+		  #100;
+		  end
+      
 endmodule
--- carry_look_ahead12/cla12_tb.v
+++ carry_look_ahead12/cla12_tb.v
@@ -1,13 +1,48 @@
-`include "cla12.v"
+`timescale 1ns / 1ps
 
-module adder12_tb;
-    reg [11:0] a, b;
-    wire [11:0] s;
-    reg carry_in;
-    wire carry_out;
-    cla12 cla12(a, b, carry_in, s, carry_out);
-    initial begin
-        carry_in = 0;
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   10:35:48 03/06/2023
+// Design Name:   cla12
+// Module Name:   C:/Users/mahdi/Desktop/cla12/cla12/cla12_tb.v
+// Project Name:  cla12
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: cla12
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module cla12_tb;
+
+	// Inputs
+	reg [11:0] a;
+	reg [11:0] b;
+	reg cin;
+
+	// Outputs
+	wire [11:0] sum;
+	wire cout;
+
+	// Instantiate the Unit Under Test (UUT)
+	cla12 uut (
+		.a(a), 
+		.b(b), 
+		.cin(cin), 
+		.sum(sum), 
+		.cout(cout)
+	);
+	initial begin 
+        cin = 0;
         a = 12'd0;
         b = 12'd0;
         #100;
@@ -20,8 +55,26 @@ module adder12_tb;
         a = 12'd7;
         b = 12'd353;
         #100;
-        a = 12'd11111;
+        a = 12'd11;
         b = 12'd3311;
         #100;
-    end
+		  cin = 1;
+		  a = 12'd0;
+        b = 12'd0;
+        #100;
+        a = 12'd15;
+        b = 12'd1795;
+        #100;
+        a = 12'd12;
+        b = 12'd14;
+        #100;
+        a = 12'd7;
+        b = 12'd353;
+        #100;
+        a = 12'd11;
+        b = 12'd3311;
+		  #100;
+		  end
+      
 endmodule
+
