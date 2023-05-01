85c519dc21fcfff401315ade2e8dbb0a7e3b1020
mohsenkamini <mohsenkamini@gmail.com>
2023-05-01 18:09:07 +0330

    added tb for ram

--- p04/memory/fuseRelaunch.cmd
+++ p04/memory/fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/mohsen/Desktop/verilog/memory/RAM_tb_isim_beh.exe" -prj "C:/Users/mohsen/Desktop/verilog/memory/RAM_tb_beh.prj" "work.RAM_tb" "work.glbl" 
+-intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "//VBOXSVR/University/Digital-Logic-Design/Lab/p04/memory/RAM_tb_isim_beh.exe" -prj "//VBOXSVR/University/Digital-Logic-Design/Lab/p04/memory/RAM_tb_beh.prj" "work.RAM_tb" "work.glbl" 
---
+++ p04/ram_gen/RAM_gen_tb.v
@@ -0,0 +1,101 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   07:45:52 04/30/2023
+// Design Name:   RAM
+// Module Name:   //VBOXSVR/University/Digital-Logic-Design/Lab/po4/memory/RAM_tb.v
+// Project Name:  memory
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: RAM
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module RAM_tb;
+
+	// Inputs
+	reg [9:0] dina;
+	reg [7:0] addra;
+
+
+	reg wea;
+	reg clk;
+
+	// Outputs
+	wire [9:0] douta;
+
+	// Instantiate the Unit Under Test (UUT)
+	RAM uut (
+		.dina(dina), 
+		.addra(addra), 
+
+
+		.wea(wea), 
+		.clk(clk), 
+		.douta(douta)
+	);
+
+	always #(20) clk = ~clk;
+	
+	initial begin
+		// Initialize Inputs
+		dina = 0;
+		addra = 0;
+
+
+		wea = 0;
+		clk = 0;
+
+		// Wait 100 ns for global reset to finish
+		#100;
+		
+		dina = 10'd29;
+		addra = 8'd0;
+
+
+		wea = 1;
+		#40;
+        
+		dina = 10'd45;
+		addra = 8'd1;
+
+
+		wea = 1;
+		#40;
+		
+		
+		dina = 10'd0;
+		addra = 8'd1;
+
+
+		wea = 0;
+		#40;
+      
+		
+
+		
+		
+		dina = 10'd0;
+		addra = 8'd1;
+
+
+		wea = 0;
+		#40;
+      
+		// Add stimulus here
+
+	end
+      
+endmodule
+
